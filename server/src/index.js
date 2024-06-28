139bf1c49bd64ca988c9c693527d426f284f7381
Shavidu Keshan <shavidu4321@gmail.com>
2024-06-28 20:44:30 +0530

    creat url api endpoint and request the url

--- server/src/index.js
+++ server/src/index.js
@@ -8,7 +8,24 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
+app.get("/getAllCurrencies" , async (req,res)=>{
+
+   const nameURL = "https://docs.openexchangerates.org/reference/currencies-json?app_id=84013cb2f52b4c6f8717b3b20e26cd33";
+
+   
+   
+   try{
+    const nameRespones = await axiox.get(nameURL);
+   const nameData = nameRespones.data;
+
+   return res.json(nameData);
+
+   }catch(err){
+    console.error(err);
+   }
+})
+
 //listen to a port
 app.listen(5000, () => {
     console.log("SERVER STARTED");
-})  
+})  ;
